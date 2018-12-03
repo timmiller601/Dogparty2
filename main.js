967ddccb610f136cbfb295f4401e11e04f856c26
tim miller <tmiller12@gmail.com>
2018-12-03 08:27:24 -0700

    final

---
+++ main.js
@@ -0,0 +1,16 @@
+// variables 
+// newName and button
+// 
+
+// EventListener 
+// button-'click'
+
+// function
+// 
+
+
+
+// var for input box to store text
+// el for button to update text
+
+document.querySelector('.head-dog').firstElementChild.innerText 
